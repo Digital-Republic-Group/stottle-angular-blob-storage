df34dabd5dff0f5f6d498d6c8f0333a4085d3d0a
Marcus Robst <20040193+mrobst@users.noreply.github.com>
2021-10-13 09:24:40 +0100

    upload enhanced blob item instead of file

--- src/app/azure-storage/services/blob-uploads-view-state.service.ts
+++ src/app/azure-storage/services/blob-uploads-view-state.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
 import { from, OperatorFunction, Subject } from 'rxjs';
 import { map, mergeMap, startWith, switchMap } from 'rxjs/operators';
+import { EnhancedBlobItem } from 'src/app/models/EnhancedBlobItem.model';
 import { BlobContainerRequest, BlobItemUpload } from '../types/azure-storage';
 import { BlobSharedViewStateService } from './blob-shared-view-state.service';
 import { BlobStorageService } from './blob-storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BlobUploadsViewStateService {
-  private uploadQueueInner$ = new Subject<FileList>();
+  // private uploadQueueInner$ = new Subject<FileList>();
+  private uploadQueueInner$ = new Subject<EnhancedBlobItem[]>();
 
+  // this gets the upload queue (as an array via the getter)
+  // and for each file triggers the upload function
   uploadedItems$ = this.uploadQueue$.pipe(
-    mergeMap(file => this.uploadFile(file)),
+    mergeMap((file) => this.uploadFile(file)),
+    // this reads the container we are uploading to and returns a list of files(?)
     this.blobState.scanEntries()
   );
 
   get uploadQueue$() {
-    return this.uploadQueueInner$
-      .asObservable()
-      .pipe(mergeMap(files => from(files)));
+    return (
+      this.uploadQueueInner$
+        .asObservable()
+        // I think this just turns an array into a stream ("from"[array])
+        .pipe(mergeMap((files) => from(files)))
+    );
   }
 
   constructor(
@@ -27,39 +35,54 @@ export class BlobUploadsViewStateService {
     private blobState: BlobSharedViewStateService
   ) {}
 
-  uploadItems(files: FileList): void {
-    this.uploadQueueInner$.next(files);
+  // uploadItems(files: FileList): void {
+  //   this.uploadQueueInner$.next(files);
+  // }
+  // uploadItems(files: File[]): void {
+  uploadItems(enhancedBlobItems: EnhancedBlobItem[]): void {
+    this.uploadQueueInner$.next(enhancedBlobItems);
   }
 
-  private uploadFile = (file: File) =>
+  // private uploadFile = (file: File) =>
+  private uploadFile = (enhancedBlobItem: EnhancedBlobItem) =>
     this.blobState.getStorageOptionsWithContainer().pipe(
-      switchMap(options =>
+      switchMap((options) =>
         this.blobStorage
-          .uploadToBlobStorage(file, {
+          .uploadToBlobStorage(enhancedBlobItem.file, {
             ...options,
-            filename: file.name + new Date().getTime()
+            //filename: file.name + new Date().getTime(),
+            // CAN CHANGE THE FILE NAME HERE!
+            // filename: file.name,
+            // file.type should be mime type and maps to content type in the blob
+            filename: enhancedBlobItem.id,
           })
           .pipe(
-            this.mapUploadResponse(file, options),
+            this.mapUploadResponse(enhancedBlobItem, options),
             this.blobState.finaliseBlobChange(options.containerName)
           )
       )
     );
 
-  private mapUploadResponse = (
-    file: File,
-    options: BlobContainerRequest
-  ): OperatorFunction<number, BlobItemUpload> => source =>
-    source.pipe(
-      map(progress => ({
-        filename: file.name,
-        containerName: options.containerName,
-        progress: parseInt(((progress / file.size) * 100).toString(), 10)
-      })),
-      startWith({
-        filename: file.name,
-        containerName: options.containerName,
-        progress: 0
-      })
-    );
+  private mapUploadResponse =
+    (
+      // file: File,
+      enhancedBlobItem: EnhancedBlobItem,
+      options: BlobContainerRequest
+    ): OperatorFunction<number, BlobItemUpload> =>
+    (source) =>
+      source.pipe(
+        map((progress) => ({
+          filename: enhancedBlobItem.id,
+          containerName: options.containerName,
+          progress: parseInt(
+            ((progress / enhancedBlobItem.file.size) * 100).toString(),
+            10
+          ),
+        })),
+        startWith({
+          filename: enhancedBlobItem.id,
+          containerName: options.containerName,
+          progress: 0,
+        })
+      );
 }
